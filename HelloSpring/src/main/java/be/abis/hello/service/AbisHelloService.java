b4e4d39ec5ef9d9d03d2d18b65e1684972192829
CecileNavez <cecile.navez@bnpparibasfortis.com>
2020-10-06 11:00:01 +0200

    add HelloSpring

---
+++ HelloSpring/src/main/java/be/abis/hello/service/AbisHelloService.java
@@ -0,0 +1,17 @@
+package be.abis.hello.service;
+
+import org.springframework.stereotype.Service;
+
+import be.abis.hello.model.Person;
+
+@Service
+public class AbisHelloService implements HelloService {
+
+	@Override
+	public Person findPerson(int id) {
+		Person p = new Person("Cecile");
+		return p;
+	}
+	
+
+}
