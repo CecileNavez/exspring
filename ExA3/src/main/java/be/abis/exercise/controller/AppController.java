a429aa37c4cedc2f0c3b7819cf024926b6357a5d
CecileNavez <cecile.navez@bnpparibasfortis.com>
2020-10-06 16:16:04 +0200

    ExA3 OK

--- ExA3/src/main/java/be/abis/exercise/controller/AppController.java
+++ ExA3/src/main/java/be/abis/exercise/controller/AppController.java
@@ -2,8 +2,10 @@ package be.abis.exercise.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import be.abis.exercise.model.Course;
 import be.abis.exercise.service.CourseService;
 
 @Controller
@@ -13,9 +15,10 @@ public class AppController {
 	CourseService courseService;
 	
 	@GetMapping("/")
-	public String showTitle() {
-		String title = courseService.findCourse(7900).getShortTitle();
-		return title;
+		public String showTitle(Model model) {
+		Course c = courseService.findCourse(7900);
+		model.addAttribute("course", c);
+		return "course";
 	}
 
 }
