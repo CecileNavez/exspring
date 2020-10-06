4d69f0b714d74101bc983289f409c03c2efe22f3
CecileNavez <cecile.navez@bnpparibasfortis.com>
2020-10-06 17:34:26 +0200

    ExA4

---
+++ ExA4/src/main/java/be/abis/exercise/controller/AppController.java
@@ -0,0 +1,25 @@
+package be.abis.exercise.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import be.abis.exercise.model.Course;
+import be.abis.exercise.service.CourseService;
+import be.abis.exercise.service.TrainingService;
+
+@Controller
+public class AppController {
+	
+	@Autowired
+	TrainingService trainingService;
+	
+	@GetMapping("/")
+		public String showTitle(Model model) {
+		String c = trainingService.getCourseService().findCourse(7900).getShortTitle();
+		model.addAttribute("coursetitle", c);
+		return "course"; // = page course.html
+	}
+
+}
