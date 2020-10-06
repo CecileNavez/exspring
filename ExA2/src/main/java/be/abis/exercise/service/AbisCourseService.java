063117b74d00c01890347742a33b38954ecd2d7b
CecileNavez <cecile.navez@bnpparibasfortis.com>
2020-10-06 14:50:15 +0200

    ExA2 c and d

--- ExA2/src/main/java/be/abis/exercise/service/AbisCourseService.java
+++ ExA2/src/main/java/be/abis/exercise/service/AbisCourseService.java
@@ -3,28 +3,30 @@ package be.abis.exercise.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import be.abis.exercise.model.Course;
-import be.abis.exercise.repository.MemoryCourseRepository;
+import be.abis.exercise.repository.CourseRepository;
 
+@Service
 public class AbisCourseService implements CourseService {
 	
 	@Autowired
-	MemoryCourseRepository m ;
+	CourseRepository courseRep ;
 	
 	@Override
 	public List<Course> findAllCourses() {
-		return m.findAllCourses();	
+		return courseRep.findAllCourses();	
 	}
 
 	@Override
 	public Course findCourse(int id) {
-		return m.findCourse(id);
+		return courseRep.findCourse(id);
 	}
 
 	@Override
 	public Course findCourse(String shortTitle) {
-		return m.findCourse(shortTitle);
+		return courseRep.findCourse(shortTitle);
 	}
 
 }
