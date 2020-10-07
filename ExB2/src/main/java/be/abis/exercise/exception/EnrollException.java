6b64000436be9659a6cccc497b24fb4dad217891
CecileNavez <cecile.navez@bnpparibasfortis.com>
2020-10-07 13:57:50 +0200

    ExB2

---
+++ ExB2/src/main/java/be/abis/exercise/exception/EnrollException.java
@@ -0,0 +1,13 @@
+package be.abis.exercise.exception;
+
+public class EnrollException extends Exception {
+
+	public EnrollException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public EnrollException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
